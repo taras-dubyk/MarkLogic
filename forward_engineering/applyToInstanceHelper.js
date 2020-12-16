317446ea7415cf840a1f9d83aac2e3f2a382efe6
Taras Dubyk <taras.dubyk@binary-studio.com>
2020-12-16 10:56:55 +0200

    add SSL handling for Apply to instance

--- forward_engineering/applyToInstanceHelper.js
+++ forward_engineering/applyToInstanceHelper.js
@@ -1,12 +1,21 @@
+const fs = require('fs');
 const marklogic = require('../reverse_engineering/node_modules/marklogic');
 
 const getDBClient = (connectionInfo, database = null) => {
+	let sslOptions = {};
+	if (connectionInfo.is_ssl) {
+		sslOptions = {
+			ssl: true,
+			...readCertificateFiles(connectionInfo),
+		};
+	}
 	return marklogic.createDatabaseClient({
 			host: connectionInfo.host,
 			port: connectionInfo.port,
 			user: connectionInfo.username,
 			password: connectionInfo.password,
 			...(database && { database }),
+			...sslOptions,
 		});
 }
 
@@ -16,6 +25,20 @@ const testConnection = dbClient => {
 
 const applyScript = (dbClient, script) => {
 	return dbClient.eval(script).result();
+}
+
+const readCertificateFiles = (connectionInfo) => {
+	const certificates = {};
+	if (connectionInfo.ca) {
+		certificates.ca = fs.readFileSync(connectionInfo.ca);
+	}
+	if (connectionInfo.sslCert) {
+		certificates.cert = fs.readFileSync(connectionInfo.sslCert);
+	}
+	if (connectionInfo.sslKey) {
+		certificates.key = fs.readFileSync(connectionInfo.sslKey);
+	}
+	return certificates;
 }
 
 module.exports = {
